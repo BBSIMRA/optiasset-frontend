581381a69d122783980af013b2bb0558b2df6ca4
BBSIMRA <bbsirah@gmail.com>
2026-04-23 00:43:58 +0530

    Fixed empty pages

---
+++ app/report-issue/page.tsx
@@ -0,0 +1,8 @@
+export default function ReportIssuePage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Report Issue</h1>
+      <p>Submit asset problems here.</p>
+    </div>
+  );
+}
